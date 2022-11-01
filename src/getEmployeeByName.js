f76c03089bd7a0ff960aeb62af785c420682b4f1
Douglas Araújo <goudxs@gmail.com>
2022-11-01 20:59:55 -0300

    Adiciona a função is manager() ao requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
+const isManager = (employeeId) => data.employees.some((employee) => employee.id === employeeId);
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
 }
