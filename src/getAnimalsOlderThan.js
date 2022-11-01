0b13f0252195c786d24f4e601680c59ffce99b93
Douglas Araújo <goudxs@gmail.com>
2022-11-01 17:35:35 -0300

    Adiciona o requisito 2 - getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const output = data.species.find((specie) => specie.name === animal);
+  return output.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
