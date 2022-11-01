c1ab82283a3fdecef380f0aa81f1ef7e82067891
Douglas Araújo <goudxs@gmail.com>
2022-11-01 15:31:09 -0300

    Adiciona o requisito 1 - getSpeciesById

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(id, ...ids) {
+  const specie = [];
+  if (id === undefined) return specie;
+  specie.push(data.species.find((animal) => animal.id === id));
+
+  if (ids !== undefined) {
+    ids.forEach((numb) => {
+      specie.push(data.species.find((animal) => animal.id === numb));
+    });
+  }
+
+  return specie;
 }
 
 module.exports = getSpeciesByIds;
