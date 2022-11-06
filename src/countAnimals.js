7effef622cb6dbe15d87ef8861f5f4c6b42efaff
Douglas Araújo <goudxs@gmail.com>
2022-11-05 22:40:24 -0300

    Adiciona o requisito 5 - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const amount = {};
+  if (animal === undefined) {
+    data.species.forEach((specie) => { amount[`${specie.name}`] = specie.residents.length; });
+    return amount;
+  }
+  const { specie, sex } = animal;
+  amount[specie] = data.species.find((elemnt) => elemnt.name === specie).residents.length;
+  if (sex !== undefined) {
+    const target = data.species.find((elemnt) => elemnt.name === specie).residents;
+    amount[specie] = target.filter((elemnt) => elemnt.sex === sex).length;
+    return amount[specie];
+  }
+  return amount[specie];
 }
 
 module.exports = countAnimals;
+console.log(countAnimals());
