a83625ced92057e694b4efdba05a3ce078750b68
Douglas Araújo <goudxs@gmail.com>
2022-11-09 23:51:53 -0300

    Adiciona o requisito 11 - getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployee(info) {
+  const employeeName = data.employees.find((elemnt) =>
+    elemnt.firstName === info.name || elemnt.lastName === info.name);
+
+  const employeeId = data.employees.find((elemnt) =>
+    elemnt.id === info.id);
+
+  if (employeeName === undefined) return employeeId;
+
+  return employeeName;
+}
+
+function generateInfo(obj) {
+  const output = {
+    id: obj.id,
+    fullName: `${obj.firstName} ${obj.lastName}`,
+    species: obj.responsibleFor.map((animalId) =>
+      data.species.find((elemnt) => elemnt.id === animalId).name),
+    locations: obj.responsibleFor.map((animalId) =>
+      data.species.find((elemnt) => elemnt.id === animalId).location),
+  };
+
+  return output;
+}
+
+function getEmployeesCoverage(obj) {
+  if (obj === undefined) {
+    const allColabs = [];
+    data.employees.forEach((elemnt) => {
+      allColabs.push(generateInfo(elemnt));
+    });
+
+    return allColabs;
+  }
+
+  if (getEmployee(obj) === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  return generateInfo(getEmployee(obj));
 }
 
 module.exports = getEmployeesCoverage;
