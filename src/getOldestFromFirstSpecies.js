d81fb012221481e46853cc61723cbe313c789a53
Douglas Araújo <goudxs@gmail.com>
2022-11-09 22:54:24 -0300

    Adiciona o requisito 10 - getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+function getColabPerson(id) {
+  const colab = data.employees.find((employee) => id === employee.id);
+
+  return colab;
+}
+
+function getFirstSpecie(id) {
+  const colab = getColabPerson(id);
+
+  const specie = [];
+
+  specie.push(data.species.find((animal) =>
+    animal.id === colab.responsibleFor[0]));
+
+  return specie;
+}
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animals = getFirstSpecie(id)[0].residents;
+
+  const oldest = animals.sort((a, b) => b.age - a.age)[0];
+
+  return Object.values(oldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
